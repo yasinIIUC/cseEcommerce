93559868ffb475b457fea86461efa490ec3eb929
yasinIIUC <ibnmonowar8@gmail.com>
2024-12-05 22:19:03 +0600

    first commit

---
+++ catalog/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+
+from .models import Item, Order, OrderItem
+
+class ItemAdmin(admin.ModelAdmin):
+    prepopulated_fields = {'slug':('title',)}
+    list_display = [
+        'title',
+        'price',
+        'disscount_price'
+
+    ]
+
+admin.site.register(Item, ItemAdmin)
+admin.site.register(Order)
+admin.site.register(OrderItem)
---
+++ catalog/models.py
@@ -0,0 +1,32 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Item(models.Model):
+    title = models.CharField(max_length=200)
+    price = models.IntegerField()
+    disscount_price = models.IntegerField(null=True, blank=True)
+    slug = models.SlugField()
+
+    def __str__(self):
+        return self.title
+
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    item = models.ForeignKey(Item, on_delete=models.CASCADE)
+    ordered = models.BooleanField(default=False)
+    quantity = models.IntegerField(default=1)
+
+    def __str__(self):
+        return f"{self.quantity} of {self.item.title}"
+
+
+class Order(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    items = models.ManyToManyField(OrderItem)
+    ordered = models.BooleanField(default=False)
+    start_date = models.DateTimeField(auto_now_add=True)
+    ordered_date = models.DateTimeField(null=True, blank=True)
+
+    def __str__(self):
+        return self.user.username
